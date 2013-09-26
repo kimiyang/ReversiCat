8c47aad07d06b6bd4605297120155d1f52921ef5
shin0123 <ryushin7@hotmail.com>
2013-09-26 15:33:53 +0800

    UI

--- ReversiCat/Form1.Designer.cs
+++ ReversiCat/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StatusLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -41,22 +43,48 @@
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 656);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Game Status:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // StatusLbl
+            // 
+            this.StatusLbl.AutoSize = true;
+            this.StatusLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLbl.Location = new System.Drawing.Point(143, 659);
+            this.StatusLbl.Name = "StatusLbl";
+            this.StatusLbl.Size = new System.Drawing.Size(51, 20);
+            this.StatusLbl.TabIndex = 2;
+            this.StatusLbl.Text = "label2";
+            // 
             // ReversiForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(663, 665);
+            this.ClientSize = new System.Drawing.Size(787, 687);
+            this.Controls.Add(this.StatusLbl);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Name = "ReversiForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.ReversiForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label StatusLbl;
     }
 }
 
--- ReversiCat/Form1.cs
+++ ReversiCat/Form1.cs
@@ -17,6 +17,8 @@ namespace ReversiCat
     {
 
         int currentPlayer = 1;   //-1 Black player 1 White player
+        bool lockProcess; //not to respond to click event if current processing is not finished yet
+        int noOfPieces = 0;
         //test test
 
         //sdfadsfa
@@ -56,9 +58,9 @@ namespace ReversiCat
         public ReversiForm()
         {
             InitializeComponent();
+            StatusLbl.Text = "White player start playing...";
             InitilizePosition();
         }
-
 
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -117,17 +119,276 @@ namespace ReversiCat
 
         private void panel1_MouseClick(object sender, MouseEventArgs e)
         {
+            if (lockProcess)
+                return;
+            else
+                lockProcess = true;
             Point hitPoint = new Point(e.X, e.Y);
             int direcX = hitPoint.X / 80;
             int direcY = hitPoint.Y / 80;
 
-            positions[direcX, direcY].color = currentPlayer;
-            currentPlayer = -currentPlayer;
-            this.Refresh();
+            if (positions[direcX, direcY].color == 0)
+            {
+                if (FlipPiece(direcX, direcY, false))
+                {
+                    noOfPieces++;
+                    positions[direcX, direcY].color = currentPlayer;
+                    currentPlayer = -currentPlayer;
+                    this.Refresh();
+                    if (noOfPieces == 64)
+                    {
+                        EndGame();
+                        return;
+                    }
+                }
 
+            }
+            int noPossibleMoves = GetNoPossibleMoves();
+            if (noPossibleMoves == 0)
+            {
+                currentPlayer = -currentPlayer;
+                
+                noPossibleMoves = GetNoPossibleMoves();
+                if (noPossibleMoves == 0)
+                {
+                    EndGame();
+                    return;
+                }
+
+                StatusLbl.Text = currentPlayer == 1 ? "No possible move for black player. White player plays again" : "No possible move for white player. Black player plays again";
+            }
+            else
+                StatusLbl.Text = currentPlayer == 1 ? "White player playing..." : "Black player playing...";
+
+            lockProcess = false;
+        }
+
+        private int GetNoPossibleMoves()
+        {
+            int result = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (positions[i, j].color == 0 && FlipPiece(i, j, true))
+                    {
+                        result ++;
+                    }
+                }
+            }
+            return result;
+        }
+
+
+        private void EndGame()
+        {
+            int result = 0;
+            foreach (Position pos in positions)
+            {
+                result += pos.color;
+            }
+            if (result > 0)
+                StatusLbl.Text = "White player won!";
+            else if (result < 0)
+                StatusLbl.Text = "Black player won!";
+            else
+                StatusLbl.Text = "Draw Game!";
+        }
+
+        private bool FlipPiece(int x, int y, bool CheckOnly)
+        {
+            List<Position> processedPosition = new List<Position>();
+            List<Position> positionToFlip = new List<Position>();
+            int i = 1;
+            bool toFlip = false;
+            //flip top left
+            while ((x- i) >= 0 && (y - i) >= 0)
+            {
+                if (positions[x - i, y - i].color == currentPlayer)
+                {
+                    toFlip = true;
+                    break;
+                }
+                else if (positions[x - i, y - i].color == 0)
+                    break;
+                else
+                    processedPosition.Add(positions[x - i, y - i]);
+                i++;
+            }
+            if (toFlip)
+            {
+                positionToFlip.AddRange(processedPosition);
+            }
+            i = 1;
+            processedPosition.Clear();
+            toFlip = false;
+
+            //flip top right
+            while ((x - i) >= 0 && (y + i) <= 7)
+            {
+                if (positions[x - i, y + i].color == currentPlayer)
+                {
+                    toFlip = true;
+                    break;
+                }
+                else if (positions[x - i, y + i].color == 0)
+                    break;
+                else
+                    processedPosition.Add(positions[x - i, y + i]);
+                i++;
+            }
+            if (toFlip)
+            {
+                positionToFlip.AddRange(processedPosition);
+            }
+            i = 1;
+            processedPosition.Clear();
+            toFlip = false;
+
+            //flip bottom left 
+            while ((x + i) <= 7 && (y - i) >= 0)
+            {
+                if (positions[x + i, y - i].color == currentPlayer)
+                {
+                    toFlip = true;
+                    break;
+                }
+                else if (positions[x + i, y - i].color == 0)
+                    break;
+                else
+                    processedPosition.Add(positions[x + i, y - i]);
+                i++;
+            }
+            if (toFlip)
+            {
+                positionToFlip.AddRange(processedPosition);
+            }
+            i = 1;
+            processedPosition.Clear();
+            toFlip = false;
+
+            //flip bottom right
+            while ((x + i) <= 7 && (y + i) <= 7)
+            {
+                if (positions[x + i, y + i].color == currentPlayer)
+                {
+                    toFlip = true;
+                    break;
+                }
+                else if (positions[x + i, y + i].color == 0)
+                    break;
+                else
+                    processedPosition.Add(positions[x + i, y + i]);
+                i++;
+            }
+            if (toFlip)
+            {
+                positionToFlip.AddRange(processedPosition);
+            }
+            i = 1;
+            processedPosition.Clear();
+            toFlip = false;
+
+            //flip top
+            while ((x - i) >= 0)
+            {
+                if (positions[x - i, y].color == currentPlayer)
+                {
+                    toFlip = true;
+                    break;
+                }
+                else if (positions[x - i, y].color == 0)
+                    break;
+                else
+                    processedPosition.Add(positions[x - i, y]);
+                i++;
+            }
+            if (toFlip)
+            {
+                positionToFlip.AddRange(processedPosition);
+            }
+            i = 1;
+            processedPosition.Clear();
+            toFlip = false;
+
+            //flip bottom
+            while ((x + i) <= 7)
+            {
+                if (positions[x + i, y].color == currentPlayer)
+                {
+                    toFlip = true;
+                    break;
+                }
+                else if (positions[x + i, y].color == 0)
+                    break;
+                else
+                    processedPosition.Add(positions[x + i, y]);
+                i++;
+            }
+            if (toFlip)
+            {
+                positionToFlip.AddRange(processedPosition);
+            }
+            i = 1;
+            processedPosition.Clear();
+            toFlip = false;
+
+            //flip left
+            while ((y - i) >= 0)
+            {
+                if (positions[x, y - i].color == currentPlayer)
+                {
+                    toFlip = true;
+                    break;
+                }
+                else if (positions[x, y - i].color == 0)
+                    break;
+                else
+                    processedPosition.Add(positions[x, y - i]);
+                i++;
+            }
+            if (toFlip)
+            {
+                positionToFlip.AddRange(processedPosition);
+            }
+            i = 1;
+            processedPosition.Clear();
+            toFlip = false;
+
+            //flip top
+            while ((y + i) <= 7)
+            {
+                if (positions[x, y + i].color == currentPlayer)
+                {
+                    toFlip = true;
+                    break;
+                }
+                else if (positions[x, y + i].color == 0)
+                    break;
+                else
+                    processedPosition.Add(positions[x, y + i]);
+                i++;
+            }
+            if (toFlip)
+            {
+                positionToFlip.AddRange(processedPosition);
+            }
+
+            if (!CheckOnly)
+            {
+                foreach (Position pos in positionToFlip)
+                    pos.color = currentPlayer;
+            }
+
+            return positionToFlip.Count > 0;
         }
 
         private void ReversiForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
